377e80d2e3809f6cdc6419e41dafb2dd08378bf0
- Edwin Mendez - <emendez@unac.edu.co>
2017-09-08 22:45:37 -0500

    method

--- app.js
+++ app.js
@@ -29,6 +29,46 @@ app.get('/createdb',(req,res) => {
     })
 });
 
+//create table
+app.get('/createpostable',(req,res) =>{
+    let sql = 'CREATE TABLE posts(id int AUTO_INCREMENT, title VARCHAR(255), body VARCHAR(255), PRIMARY KEY (id))';
+    db.query(sql,(err, result) =>{
+        if(err) throw err;
+        res.send('Post table created.....'); 
+    });
+});
+
+//insert post 1
+app.get('/addpost1', (req,res) => {
+    let post = {title:'Post One',body:'this is post number one'};
+    let sql = 'INSERT INTO posts SET?';
+    let query = db.query(sql, post,(err, result) =>{
+        if(err) throw err;
+        console.log(result);
+        res.send('Post 1 added...');
+    });
+});
+
+//select post
+app.get('/getposts', (req,res) => {
+    let sql = 'SELECT * FROM posts';
+    let query = db.query(sql,(err, result) =>{
+        if(err) throw err;
+        console.log(result);
+        res.send('Post fetched...');
+    });
+});
+
+//select single post
+app.get('/getpost/:id', (req,res) => {
+    let sql = 'SELECT * FROM posts WHERE id => ${req.params.id}';
+    let query = db.query(sql,(err, result) =>{
+        if(err) throw err;
+        console.log(result);
+        res.send('Postfetched...');
+    });
+});
+
 app.listen('3000',() => { 
     console.log('Server started on por 3000');
 });
